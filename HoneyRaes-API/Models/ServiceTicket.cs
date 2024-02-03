bf93dee35491c1df2cff587c8e125cb4ac8a5846
Dylan Moore <dylankmoore@gmail.com>
2024-02-02 21:14:09 -0600

    finished extra endpoints

--- HoneyRaes-API/Models/ServiceTicket.cs
+++ HoneyRaes-API/Models/ServiceTicket.cs
@@ -4,10 +4,10 @@ public class ServiceTicket
 {
     public int Id { get; set; }
     public int CustomerId { get; set; }
-    public int EmployeeId { get; set; }
-    public string Description { get; set; }
+    public int? EmployeeId { get; set; }
+    public string? Description { get; set; }
     public bool Emergency { get; set; }
-    public DateTime DateCompleted { get; set; }
-    public Employee Employee { get; set; }
-    public Customer Customer { get; set; }
+    public DateTime? DateCompleted { get; set; }
+    public Employee? Employee { get; set; }
+    public Customer? Customer { get; set; }
 }
